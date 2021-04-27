9087ea4762d9ec6a3c7b5a713bda68f7ebedf2a2
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-04-27 17:58:14 -0500

    commit tree ee1e998138f2a6c9bb9b2e6ccddea2c5a89954c7

--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import QuoteCard from '../components/QuoteCard';
+import {upvoteQuote, downvoteQuote, removeQuote} from '../actions/quotes'
 
 class Quotes extends Component {
+
+  renderQuotes = () => this.props.quotes.map(quote => <QuoteCard key={quote.id} quote={quote} upvoteQuote={this.props.upvoteQuote} downvoteQuote={this.props.downvoteQuote} removeQuote={this.props.removeQuote} />)
 
   render() {
     return (
@@ -20,6 +23,7 @@ class Quotes extends Component {
 
                 Render Quotes With QuoteCard component and pass down callback props for removing, upvoting and downvoting quotes
                */}
+              {this.renderQuotes()}
             </div>
           </div>
         </div>
@@ -29,4 +33,10 @@ class Quotes extends Component {
 }
 
 //add arguments to connect as needed
-export default connect()(Quotes);
+const mapStateToProps = ({quotes}) => ({quotes})
+
+// const mapDispatchToProps = dispatch => {
+//   upvoteQuote: quoteId => dispatch(upvoteQuote(quoteId))
+// }
+
+export default connect(mapStateToProps, {upvoteQuote, removeQuote, downvoteQuote})(Quotes);
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,37 @@
+// import uuid from 'uuid'
+
 export default (state = [], action) => {
-  return state;
+  // debugger
+  let idx = state.findIndex(quote => quote.id === action.quoteId)
+  
+  switch(action.type){
+    case "ADD_QUOTE" :
+      // const quote = Object.assign(action.quote, {votes: 0})
+      return state.concat(action.quote)
+
+    case "REMOVE_QUOTE" :
+      // debugger
+      return state.filter(quote => quote.id !== action.quoteId)
+
+    case "UPVOTE_QUOTE" :
+      // debugger
+      
+      
+      // Object.assign(state[idx], {votes: state[idx].votes + 1})
+      state = [...state.slice(0,idx), Object.assign({}, state[idx], {votes: state[idx].votes + 1}), ...state.slice(idx + 1)]
+      // debugger
+      return state
+
+    case "DOWNVOTE_QUOTE" :
+      // const downvotedQuote = state.find(quote => quote.id === action.quoteId)
+      // idx = state.fin
+      if(state[idx].votes > 0){
+        // Object.assign(downvotedQuote, {votes: downvotedQuote.votes - 1})
+        state = [...state.slice(0,idx), Object.assign({}, state[idx], {votes: state[idx].votes - 1}), ...state.slice(idx + 1)]
+      }
+      return state
+    
+    default:
+      return state
+  }
 }
